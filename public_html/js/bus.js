622361c6d15d00d4bf1b6ad40b1e73d50fc0ff92
JaimeCabrera <jycabrerap@unl.edu.ec>
2018-02-15 09:46:35 -0500

    mapbox
    agregado el mapa de rutas usando mapbox

--- public_html/js/bus.js
+++ public_html/js/bus.js
@@ -29,11 +29,10 @@ function validar() {
     }
 }
 function limpiar() {
-    $('#limpiar').click(function () {
-
+    
         $('input[type="text"]').val('');
         $('#alertaLugar').hide();
-    });
+    
 }
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
