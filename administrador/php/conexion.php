4b585178232fa93e3b19962aaf98fde43df8608c
JaimeCabrera <jycabrerap@unl.edu.ec>
2018-02-28 08:21:18 -0500

    login administrador
    login del administrador aun falta crear los menus y la cosas que va hacer el admin

---
+++ administrador/php/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="blimp";
+$con = new mysqli($host,$user,$password,$db);
+
+?>
---
+++ administrador/php/footer.php
@@ -0,0 +1,5 @@
+<div class="panel-footer" id="footer">
+	<h4>Universidad Nacional de Loja</h4>
+	<h5>Carrera de Ingeniería en Sistemas</h5>
+	<h5></h5>
+</div>
