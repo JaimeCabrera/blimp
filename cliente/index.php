f5b8420d1e59810a7c7238b18733703e2f72ae3d
Guido Quezada <gqrott@gmail.com>
2018-03-08 04:34:49 -0500

    Nuevos estilos

--- cliente/index.php
+++ cliente/index.php
@@ -1,95 +1,222 @@
-
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>BusLineMap</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/stilos.css">
-
-        <script type="text/javascript" src="js/mapbox-gl.js"></script>
-        <link href="css/mapbox-gl.css" rel="stylesheet" />
-        <script type="text/javascript" src="js/bus.js"></script>
-        <script type="text/javascript" src="js/busqueda.js"></script>
-        <script type="text/javascript">
-        function clicks(valor) {
-
-          location.href='rutas.php?variable='+valor;
-        }
-
-
-
-        </script>
-    </head>
-
-    <body  >
-        <br>
-        <div class="container-fluid">
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="index.php">BusLineMap</a>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bus Line Map</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/busqueda.js"></script>
+    <script type="text/javascript">
+      function clicks(valor) {
+        location.href='rutas.php?variable='+valor;
+      }
+    </script>
+</head>
+<body id="page-top" class="index">
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span><i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php#page-top">BusLineMap</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="index.php#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="rutas.php">Rutas</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="horarios.php">Horarios</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="index.php#nosotros">Nosotros</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="index.php#contacto">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="assets/logo.svg" alt="">
+                    <div class="intro-text">
+                        <span class="skills">lema chido vendedor </span>
                     </div>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav nav">
-                            <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-                            <li><a href="rutas.php">Ver todas las rutas</a></li>
-                            <li><a href="horarios.php">Ver los Horarios</a></li>
-                        </ul>
-                        <form class="navbar-form navbar-right">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header -->
+    <section class="success" id="search">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-offset-1 col-lg-10">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <label class="btn btn-default" >Buscar!</label>
+                </span>
+                <input type="text" class="form-control" placeholder="Escriba lugar de destino..."  name="caja_busqueda" id="caja_busqueda">
+              </div>
+           </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-offset-1 col-lg-10" id="datos"></div>
+          </div>
+        </div>
+    </section>
+    <!-- Nosotros Section -->
+    <section class="success" id="nosotros">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Quiénes somos</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p style="text-align:justify">
+Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <p style="text-align:justify">ra que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section -->
+    <section id="contacto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contáctanos</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Nombre</label>
+                                <input type="text" class="form-control" placeholder="Nombre" id="name" required data-validation-required-message="Por favor ingrese su nombre">
+                                <p class="help-block text-danger"></p>
                             </div>
-                            <button type="submit" class="btn btn-default">Buscar</button>
-                        </form>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email</label>
+                                <input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Por favor ingrese su email.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Teléfono</label>
+                                <input type="tel" class="form-control" placeholder="Teléfono" id="phone" required data-validation-required-message="Por favor ingrese su teléfono">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Mensaje</label>
+                                <textarea rows="5" class="form-control" placeholder="Mensaje" id="message" required data-validation-required-message="Por favor ingrese su mensaje"></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-success btn-lg">Enviar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <!--pagina-->
-        <div id="pagina">
+    </section>
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
             <div class="container">
-                <div class="page-header">
-                    <div class="jumbotron">
-                        <h2> Logotipo y mas Informacion de la aplicacion</h2>
-                        <p>...</p>
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Ubicación</h3>
+						Av. Pío Jaramillo Alvarado y Reinaldo Espinosa, La Argelia
+						Loja – Ecuador
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Redes sociales</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="https://www.facebook.com/universidadnacionaloja" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                                <a href="https://twitter.com/nacionaldeloja" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Acerca de moviggo</h3>
+                        serviciocliente@blimp.com<br>
+						Teléfono (593) (07) 258-8669 ext. 128
                     </div>
                 </div>
-                <div class="col-lg-6">
-                  <div class="input-group">
-                    <span class="input-group-btn">
-                      <label class="btn btn-default" >Buscar!</label>
-                    </span>
-                    <input type="text" class="form-control" placeholder="Escriba lugar de destino..."  name="caja_busqueda" id="caja_busqueda">
-                  </div>
-               </div> </br> </br>
-             </br>
-                <div id="datos">
-
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; blimp 2016. Powered by: <a href="http://desarrollotroll.com/" style="color:white; font-weight:bold; text-decoration:underline">desarrollotroll</a>
+                    </div>
                 </div>
-                
-
-
+            </div>
         </div>
-        <!--fin de la pagina-->
-        <br>
-        <!--Pie de la PAgina-->
-
-        <div class="panel-footer" id="footer">
-            <h4>Universidad Nacional de Loja</h4>
-            <h5>Carrera de Ingeniería en Sistemas</h5>
-            <h5></h5>
-        </div>
-    </body>
+    </footer>
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="index.php#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+</body>
 </html>
